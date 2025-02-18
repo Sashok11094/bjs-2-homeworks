4706a2f208db353084679054fd68d8c33747434b
Alexander Kuznetsov <saskakuzneu@gmail.com>
2025-02-18 15:26:14 +0300

    First task is ready

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,27 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
+ let arr = [];
+  let D =  b ** 2 - 4 * a * c;
+  if (D > 0) {
+    let root1 = (-b + Math.sqrt(D) )/(2*a);
+    let root2 = (-b - Math.sqrt(D) )/(2*a);
+    return [root1, root2];
+  } else if (D === 0) {
+    let root = -b/(2*a);
+    return [root];
+  } else {
+    // Нет корней!
+    return [];
+  }
   
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let interestRate = percent / 100 / 12;
+  let creditBody = amount - contribution;
+  let monthlyPayment = creditBody * (interestRate + (interestRate / (((1 + interestRate) ** countMonths) - 1)));
+  let totalPayment = monthlyPayment * countMonths;
+  totalPayment = Math.round(totalPayment * 100) / 100;
+  return totalPayment;
 }
